2acc99daf149dd406b6b2ec32a12fc51209e577a
Mitch Gildenberg <mitch@switchboard.xyz>
2024-03-25 08:03:45 -0400

    init

--- sb-on-demand-solana/programs/sb-on-demand-solana/src/lib.rs
+++ sb-on-demand-solana/programs/sb-on-demand-solana/src/lib.rs
@@ -7,7 +7,7 @@ declare_id!("84LSTTNKxiQFey1mx4b2C74GKrSKMcuJZYrnL8J9wAaT");
 pub mod sb_on_demand_solana {
     use super::*;
 
-    pub fn test(ctx: Context<Initialize>) -> Result<()> {
+    pub fn test(ctx: Context<Test>) -> Result<()> {
         // Docs at: https://switchboard-on-demand-rust-docs.web.app/on_demand/accounts/pull_feed/struct.PullFeedAccountData.html#method.get_value
         let feed = PullFeedAccountData::parse(ctx.accounts.feed.clone()).unwrap();
         let price = feed.get_value(&Clock::get()?, 30, 1, true).unwrap();

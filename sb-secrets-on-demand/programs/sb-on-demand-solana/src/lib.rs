aee99e8cf5af23b20a68f15ac46feafddfd7ad30
Astewart1510 <astewart1510@gmail.com>
2024-04-15 20:56:14 -0600

    sb-secrets: in-progress

---
+++ sb-secrets-on-demand/programs/sb-on-demand-solana/src/lib.rs
@@ -0,0 +1,26 @@
+use anchor_lang::prelude::*;
+use switchboard_on_demand::on_demand::accounts::pull_feed::PullFeedAccountData;
+
+declare_id!("2nw6drdFZVEnJQvDU2WQ65t9bfuXYD6eft7yBEq85R2a");
+
+#[program]
+pub mod sb_on_demand_solana {
+    use super::*;
+
+    pub fn test<'a>(ctx: Context<Test>) -> Result<()> {
+        let feed_account = ctx.accounts.feed.data.borrow();
+        // Docs at: https://switchboard-on-demand-rust-docs.web.app/on_demand/accounts/pull_feed/struct.PullFeedAccountData.html#method.get_value
+        let feed = PullFeedAccountData::parse(feed_account)
+            .map_err(|_e| ProgramError::Custom(1))?;
+        let price = feed.get_value(&Clock::get()?, 30, 1, true)
+            .map_err(|_e| ProgramError::Custom(2))?;
+        msg!("price: {:?}", price.mantissa());
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Test<'info> {
+    /// CHECK: via switchboard sdk
+    pub feed: AccountInfo<'info>,
+}
--- sb-secrets-on-demand/scripts/index.ts
+++ sb-secrets-on-demand/scripts/index.ts
@@ -47,6 +47,15 @@ function loadDefaultKeypair() {
     return OracleJob.create({ tasks });
   }
 
+async function myAnchorProgram(
+    provider: anchor.Provider
+  ): Promise<anchor.Program> {
+    const myPid = new PublicKey("2nw6drdFZVEnJQvDU2WQ65t9bfuXYD6eft7yBEq85R2a");
+    const idl = (await anchor.Program.fetchIdl(myPid, provider))!;
+    const program = new anchor.Program(idl, myPid, provider);
+    return program;
+  }
+
 (async () => {
     dotenv.config();
     const PID = sb.SB_ON_DEMAND_PID;
@@ -60,7 +69,7 @@ function loadDefaultKeypair() {
       );
     const provider = new anchor.AnchorProvider(connection, wallet, {});
     const idl = (await anchor.Program.fetchIdl(PID, provider))!;
-    const program = new anchor.Program(idl, PID, provider);
+    const sbProgram= new anchor.Program(idl, PID, provider);
     
     const sbSecrets = new SwitchboardSecrets();
     console.log("\n🔒 Step 1: Creating the User for Secrets");
@@ -80,7 +89,7 @@ function loadDefaultKeypair() {
           );
         console.log("User created",user);
     }
-    const secretName = "OPEN_WEATHER_API_KEY_TEST";
+    const secretName = "OPEN_WEATHER_API_KEY";
     const secretValue =  API_KEY ?? "API_KEY_NOT_FOUND";
     console.log("\n🔒 Step 2: Checking and Creating the Secret");
 
@@ -119,11 +128,11 @@ function loadDefaultKeypair() {
         console.error("Invalid feed secret key");
         return;
     }
-    const pullFeed = new PullFeed(program , feedKeypair.publicKey);
+    const pullFeed = new PullFeed(sbProgram, feedKeypair.publicKey);
     // config for the feed
     const conf: any = {
         queue,
-        jobs: [buildOpenWeatherAPI("Aspen", "OPEN_WEATHER_API_KEY_TEST")],
+        jobs: [buildOpenWeatherAPI("Aspen", "OPEN_WEATHER_API_KEY")],
         maxVariance: 1.0,
         minResponses: 1,
         numSignatures: 1,
@@ -132,7 +141,7 @@ function loadDefaultKeypair() {
     
     while (true) {
         try {
-            conf.feedHash = await Queue.fetchFeedHash(program, conf);
+            conf.feedHash = await Queue.fetchFeedHash(sbProgram, conf);
             console.log("Successfully fetched Feed Hash:", conf.feedHash);
             break
         } catch (error) {
@@ -141,27 +150,64 @@ function loadDefaultKeypair() {
         }
     }
 
-    // const deleteSecret = await sbSecrets.createDeleteSecretRequest(wallet.publicKey.toBase58(), "ed25519", secretName);
-    // console.log("Delete Secret Request",deleteSecret);
-    // const deleteSecretSignature = nacl.sign.detached(
-    //     new Uint8Array(deleteSecret.toEncodedMessage()),
+    // whitelist the secret to the feed hash
+    const feedHash = createHash("sha256").update(conf.feedHash).digest("hex")
+      
+    console.log("feedHash",feedHash);
+    // commented out because the feed hash is already whitelisted
+
+    // const addwhitelist = await sbSecrets.createAddMrEnclaveRequest(wallet.payer.publicKey.toBase58(), "ed25519", feedHash, [secretName]);
+    // const whitelistSignature = nacl.sign.detached(
+    //     new Uint8Array(addwhitelist.toEncodedMessage()),
     //     wallet.payer.secretKey
     // );
-    // const deletedSecret = await sbSecrets.deleteSecret(deleteSecret, Buffer.from(deleteSecretSignature).toString("base64"));
-    // console.log("Deleted Secret",deletedSecret);
-    // const getSecret = await sbSecrets.getUserSecrets(wallet.publicKey.toString(), "ed25519");
-    // console.log("Secrets: ",getSecret);
-    const addwhitelist = await sbSecrets.createAddMrEnclaveRequest(wallet.payer.publicKey.toBase58(), "ed25519", conf.feedHash , [secretName]);
-    const whitelistSignature = nacl.sign.detached(
-        new Uint8Array(addwhitelist.toEncodedMessage()),
-        wallet.payer.secretKey
-    );
-    const sendwhitelist = await sbSecrets.addMrEnclave(
-        addwhitelist, 
-        Buffer.from(whitelistSignature).toString("base64"));
+    // const sendwhitelist = await sbSecrets.addMrEnclave(
+    //     addwhitelist, 
+    //     Buffer.from(whitelistSignature).toString("base64"));
     
-    console.log("sendwhitelist",sendwhitelist);
+    // console.log("sendwhitelist",sendwhitelist);
+    
     const getuserSecrets = await sbSecrets.getUserSecrets(wallet.payer.publicKey.toBase58(), "ed25519");
     console.log("getuserSecrets",getuserSecrets)
 
+    // initialize the feed
+    const ix = await pullFeed.initIx(conf);
+    const tx = await InstructionUtils.asV0Tx(sbProgram, [ix]);
+    tx.sign([wallet.payer, feedKeypair]);
+    const sig = await connection.sendTransaction(tx, {
+        preflightCommitment: "processed",
+    });
+    await connection.confirmTransaction(sig);
+    console.log("Feed initialized: ", sig);
+
+    const myProgram = await myAnchorProgram(provider);
+
+    while (true) {
+        try {
+          const tx = await InstructionUtils.asV0Tx(sbProgram, [
+            await pullFeed.solanaFetchUpdateIx(conf),
+            await myProgram.methods
+              .test()
+              .accounts({ feed: feedKeypair.publicKey })
+              .instruction(),
+          ]);
+          tx.sign([wallet.payer]);
+          const sim = await connection.simulateTransaction(tx, {
+            commitment: "processed",
+          });
+          console.log(
+            "Simulated update: ",
+            sim?.value?.logs?.filter((x) => x.includes("Program log:"))
+              .filter((x) => !x.includes("Instruction:"))
+          );
+          const sig = await connection.sendTransaction(tx, {
+            skipPreflight: true,
+          });
+          console.log("Sent update signature: ", sig);
+        } catch (e) {
+          console.log(e);
+        }
+        await sleep(5_000);
+      }
+      return;
 })();

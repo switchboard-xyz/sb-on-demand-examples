5cdb8b0de582e424938acdd73df4377cdfb3d22c
Astewart1510 <astewart1510@gmail.com>
2024-06-06 15:11:19 -0600

    social metric twitter example

---
+++ sb-on-demand-secret/sb-on-demand-secrets-socials/programs/sb-on-demand-solana/src/lib.rs
@@ -0,0 +1,42 @@
+use anchor_lang::prelude::*;
+use switchboard_on_demand::on_demand::accounts::pull_feed::PullFeedAccountData;
+
+declare_id!("96QC5EZGi8eLkvwXARh5gGr7cdpbZCzexhBerYUUNXm6");
+
+fn fmt(s: &str) -> String {
+    if s.len() < 18 {
+        // Handle error or return the original string if it's less than 18 characters
+        return s.to_string();
+    }
+    let split_index = s.len() - 18;
+    let (first_part, last_part) = s.split_at(split_index);
+    format!("{}.{}", first_part, last_part)
+}
+
+#[program]
+pub mod sb_on_demand_solana {
+    use super::*;
+
+    pub fn test<'a>(ctx: Context<Test>) -> Result<()> {
+        let feed_account = ctx.accounts.feed.data.borrow();
+        // Docs at: https://switchboard-on-demand-rust-docs.web.app/on_demand/accounts/pull_feed/struct.PullFeedAccountData.html#method.get_value
+        let feed = PullFeedAccountData::parse(feed_account)
+            .map_err(|e| {
+                msg!("Parse Error: {:?}", e);
+                ProgramError::Custom(1)}
+            )?;
+        let temperature = feed.get_value(&Clock::get()?, 30, 1, true)
+            .map_err(|e| {
+                msg!("Get Value Error: {:?}", e);
+                ProgramError::Custom(2)
+            })?;
+        msg!("Social: Followers Count on X.com : {:?}", fmt(&temperature.mantissa().to_string()));
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Test<'info> {
+    /// CHECK: via switchboard sdk
+    pub feed: AccountInfo<'info>,
+}

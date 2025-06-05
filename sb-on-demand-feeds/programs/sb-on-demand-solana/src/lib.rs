df93410b49bfe6c17851d137367515e7da8138b4
Mitch Gildenberg <mitch@switchboard.xyz>
2025-06-05 03:06:47 -0400

    clean repo

--- sb-on-demand-feeds/programs/sb-on-demand-solana/src/lib.rs
+++ sb-on-demand-feeds/programs/sb-on-demand-solana/src/lib.rs
@@ -11,7 +11,7 @@ pub mod sb_on_demand_solana {
 
     pub fn test<'a>(ctx: Context<Ctx>, bundle: Vec<u8>) -> Result<()> {
         let Ctx { queue, slothashes, instructions } = ctx.accounts;
-        let verified_bundle = BundleVerifierBuilder::default()
+        let verified_bundle = BundleVerifierBuilder::new()
             .queue(&queue.to_account_info())
             .slothash_sysvar(&slothashes.to_account_info())
             .ix_sysvar(&instructions.to_account_info())

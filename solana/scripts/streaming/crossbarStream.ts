2f5f358aef41f4eeaffbe9863b9557544f91718c
Mitch Gildenberg <mitch@switchboard.xyz>
2025-07-24 17:42:52 -0400

    sdk fix

--- solana/scripts/streaming/crossbarStream.ts
+++ solana/scripts/streaming/crossbarStream.ts
@@ -5,8 +5,8 @@ import * as sb from "@switchboard-xyz/on-demand";
 
   const surge = new sb.Surge({
     apiKey: apiKey,
-    crossbarUrl: 'http://localhost:8080',
-    // crossbarUrl: 'https://staging.crossbar.switchboard.xyz',
+    // crossbarUrl: 'http://localhost:8080',
+    crossbarUrl: 'https://staging.crossbar.switchboard.xyz',
     crossbarMode: true,
     verbose: true,
   });
@@ -16,22 +16,23 @@ import * as sb from "@switchboard-xyz/on-demand";
     const symbols = update.getSymbols();
     const sources = update.getSources();
     const formattedPrices = update.getFormattedPrices();
-    // console.log(`\nReceived unsigned price update for ${JSON.stringify(update)}`);
+    console.log(`\nReceived unsigned price update for ${JSON.stringify(update)}`);
 
-    update.rawResponse.feed_values.forEach((feedValue: any) => {
-      const symbol = feedValue.symbol;
-      const latency = Date.now() - feedValue.seen_at_ts_ms;
-      console.log(`\nReceived unsigned price update for ${symbol}:`);
-      const latencyInfo = ` | Latency: ${latency}ms`;
-      console.log(`${symbol} (${sources[0]}): ${formattedPrices[symbol]}${latencyInfo}`);
-    });
+    // update.rawResponse.feed_values.forEach((feedValue: any) => {
+      // const symbol = feedValue.symbol;
+      // const latency = Date.now() - feedValue.seen_at_ts_ms;
+      // console.log(`\nReceived unsigned price update for ${symbol}:`);
+      // const latencyInfo = ` | Latency: ${latency}ms`;
+      // console.log(`${symbol} (${sources[0]}): ${formattedPrices[symbol]}${latencyInfo}`);
+    // });
   });
 
   // Connect and subscribe
-  await surge.connect();
-  await surge.subscribeToAll();
-  // await surge.connectAndSubscribe([
-    // { symbol: 'BTC/USD' }
-  // ]);
+  // await surge.connect();
+  // await surge.subscribeToAll();
+  await surge.connectAndSubscribe([
+    { symbol: 'BTC/USD' },
+    { symbol: 'ETH/USD' },
+  ]);
   console.log('🎧 Streaming prices...\n');
 })()

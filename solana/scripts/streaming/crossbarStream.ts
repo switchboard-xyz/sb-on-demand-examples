9f092dd501686ec02b4a81df104acb9fce7cbf70
Astewart1510 <astewart1510@gmail.com>
2025-08-06 15:59:44 +0200

    update

--- solana/scripts/streaming/crossbarStream.ts
+++ solana/scripts/streaming/crossbarStream.ts
@@ -5,7 +5,6 @@ import * as sb from "@switchboard-xyz/on-demand";
 
   const surge = new sb.Surge({
     apiKey: apiKey,
-    // crossbarUrl: 'http://localhost:8080',
     crossbarUrl: 'https://staging.crossbar.switchboard.xyz',
     crossbarMode: true,
     verbose: true,
@@ -16,20 +15,19 @@ import * as sb from "@switchboard-xyz/on-demand";
     const symbols = update.getSymbols();
     const sources = update.getSources();
     const formattedPrices = update.getFormattedPrices();
-    console.log(`\nReceived unsigned price update for ${JSON.stringify(update)}`);
+    // Uncomment the line below to see the full update object
+    // console.log(`\nReceived unsigned price update for ${JSON.stringify(update)}`);
 
-    // update.rawResponse.feed_values.forEach((feedValue: any) => {
-      // const symbol = feedValue.symbol;
-      // const latency = Date.now() - feedValue.seen_at_ts_ms;
-      // console.log(`\nReceived unsigned price update for ${symbol}:`);
-      // const latencyInfo = ` | Latency: ${latency}ms`;
-      // console.log(`${symbol} (${sources[0]}): ${formattedPrices[symbol]}${latencyInfo}`);
-    // });
+    update.rawResponse.feed_values.forEach((feedValue: any) => {
+      const symbol = feedValue.symbol;
+      const latency = Date.now() - feedValue.seen_at_ts_ms;
+      console.log(`\nReceived unsigned price update for ${symbol}:`);
+      const latencyInfo = ` | Latency: ${latency}ms`;
+      console.log(`${symbol} (${sources[0]}): ${formattedPrices[symbol]}${latencyInfo}`);
+    });
   });
 
   // Connect and subscribe
-  // await surge.connect();
-  // await surge.subscribeToAll();
   await surge.connectAndSubscribe([
     { symbol: 'BTC/USD' },
     { symbol: 'ETH/USD' },

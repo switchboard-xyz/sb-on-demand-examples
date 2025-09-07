85954e114028f0ffed7bf6cce00941f92146a1af
Mitch Gildenberg <mitch@switchboard.xyz>
2025-09-06 22:47:37 -0400

    checkpoint 0.7 update

--- solana/programs/sb-on-demand-solana/src/lib.rs
+++ solana/programs/sb-on-demand-solana/src/lib.rs
@@ -9,32 +9,36 @@ pub mod sb_on_demand_solana {
     use super::*;
 
     pub fn verify(ctx: Context<VerifyCtx>) -> Result<()> {
-        let VerifyCtx { queue, slothashes, oracle, .. } = ctx.accounts;
+        let clock = Clock::get()?;
+        let VerifyCtx { queue, slothashes, oracle, instructions, clock, .. } =
+            ctx.accounts;
 
         anchor_lang::solana_program::log::sol_log_compute_units();
-        let bundle = QuoteVerifierBuilder::new()
-            .queue(&queue)
+        let quote = QuoteVerifierBuilder::new()
+            .queue(queue.as_ref())
             .slothash_sysvar(&slothashes)
-            .max_age(50)
-            .verify_account(oracle.as_ref())
+            .ix_sysvar(instructions.as_ref())
+            .verify_account(oracle)
             .unwrap();
         anchor_lang::solana_program::log::sol_log_compute_units();
 
-        anchor_lang::solana_program::msg!("Verified bundle slot: {}", bundle.slot());
-        for feed_info in bundle.feeds() {
-            msg!("Feed ID: {}, value: {}", feed_info.hex_id(), feed_info.value());
-        }
+        // anchor_lang::solana_program::msg!("Verified bundle slot: {}", bundle.slot());
+        // for feed_info in bundle.feeds() {
+            // msg!("Feed ID: {}, value: {}", feed_info.hex_id(), feed_info.value());
+        // }
         Ok(())
     }
 
     pub fn switchboard_oracle_update(ctx: Context<UpdateCtx>) -> Result<()> {
+        let clock = Clock::get()?;
         let UpdateCtx { state, instructions, clock, payer, oracle, .. } = ctx.accounts;
         let cranker = state.cranker.get_or_insert(payer.key());
 
         // Only allow the cranker to call this function
         require!(check_pubkey_eq(&cranker, payer.key), ErrorCode::ConstraintSigner);
         anchor_lang::solana_program::log::sol_log_compute_units();
-        OracleQuote::write_ix_0_to_account_info(instructions.as_ref(), clock, oracle.as_ref());
+        let (pid, quote) = Instructions::extract_ix_data(instructions.as_ref(), 0);
+        OracleQuote::write_to_account(clock, quote, oracle);
         anchor_lang::solana_program::log::sol_log_compute_units();
         Ok(())
     }
@@ -50,34 +54,28 @@ impl ProgramState {
 
 #[derive(Accounts)]
 pub struct VerifyCtx<'info> {
-    #[account(mut, seeds = [b"state"], bump)]
-    pub state: Account<'info, ProgramState>,
     /// CHECK: internal
     #[account(seeds = [b"oracle"], bump)]
-    pub oracle: AccountInfo<'info>,
-    pub queue: AccountLoader<'info, QueueAccountData>,
-    pub slothashes: Sysvar<'info, SlotHashes>,
-    pub payer: Signer<'info>,
+    pub oracle:       AccountInfo<'info>,
+    #[account(mut, seeds = [b"state"], bump)]
+    pub state:        Account<'info, ProgramState>,
+    pub queue:        AccountLoader<'info, QueueAccountData>,
+    pub slothashes:   Sysvar<'info, SlotHashes>,
+    pub instructions: Sysvar<'info, Instructions>,
+    pub clock:        Sysvar<'info, Clock>,
+    pub payer:        Signer<'info>,
 }
 
 #[derive(Accounts)]
 pub struct UpdateCtx<'info> {
-    #[account(
-        init_if_needed,
-        seeds = [b"state"],
-        bump,
-        space = ProgramState::LEN,
-        payer = payer,
-    )]
-    pub state: Account<'info, ProgramState>,
     /// CHECK: internal
     #[account(init_if_needed, seeds = [b"oracle"], bump, space = 1024, payer = payer)]
-    pub oracle: AccountInfo<'info>,
-    /// CHECK: internal
-    pub instructions: Sysvar<'info, Instructions>,
-    /// CHECK: solana_program::sysvar::clock::ID
-    pub clock: AccountInfo<'info>,
+    pub oracle:         AccountInfo<'info>,
+    #[account(init_if_needed, seeds = [b"state"], bump, space = 312, payer = payer)]
+    pub state:          Account<'info, ProgramState>,
+    pub instructions:   Sysvar<'info, Instructions>,
+    pub clock:          Sysvar<'info, Clock>,
     #[account(mut)]
-    pub payer: Signer<'info>,
+    pub payer:          Signer<'info>,
     pub system_program: Program<'info, System>,
 }

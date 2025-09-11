a98ab1960c9ad456373ca00ae91246da6fa29873
Mitch Gildenberg <mitch@switchboard.xyz>
2025-09-11 15:53:04 -0400

    cp

--- solana/scripts/feeds/legacy/runJob.ts
+++ solana/scripts/feeds/legacy/runJob.ts
@@ -1,15 +1,12 @@
 import { OracleJob, CrossbarClient } from "@switchboard-xyz/common";
 import * as sb from "@switchboard-xyz/on-demand";
-import yargs from "yargs";
-import { sleep, calculateStatistics } from "../../utils";
 
 
 (async function main() {
   const { keypair, connection, program } = await sb.AnchorUtils.loadEnv();
   const queue = await sb.Queue.loadDefault(program!);
-  // const crossbar = new CrossbarClient("http://localhost:8080");
   const crossbar = new CrossbarClient("http://crossbar.switchboard.xyz");
-  const gateway = await queue.fetchGatewayFromCrossbar(crossbar as any);
+  const gateway = await queue.fetchGatewayFromCrossbar(crossbar);
   const res = await queue.fetchSignaturesConsensus({
     gateway: "http://localhost:8082",
     feedConfigs: [{

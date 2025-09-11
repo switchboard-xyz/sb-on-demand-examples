4fdc276c4ff7cc863d3a3b8ca3109e9136b65f99
Mitch Gildenberg <mitch@switchboard.xyz>
2025-09-11 14:51:54 -0400

    cp

--- solana/scripts/feeds/legacy/runJob.ts
+++ solana/scripts/feeds/legacy/runJob.ts
@@ -2,71 +2,6 @@ import { OracleJob, CrossbarClient } from "@switchboard-xyz/common";
 import * as sb from "@switchboard-xyz/on-demand";
 import yargs from "yargs";
 import { sleep, calculateStatistics } from "../../utils";
-import { buildBinanceJob } from "../../utils";
-
-interface JobConfig {
-  name: string;
-  builder: (param: string) => OracleJob;
-  paramName: string;
-  description: string;
-}
-
-const JOB_CONFIGS: JobConfig[] = [
-  {
-    name: "binance",
-    builder: buildBinanceJob,
-    paramName: "pair",
-    description: "Binance price feed (e.g., BTCUSDT)"
-  }
-];
-
-const argv = yargs(process.argv)
-  .options({
-    job: {
-      required: true,
-      type: 'string',
-      describe: 'Job type to run',
-      choices: JOB_CONFIGS.map(config => config.name)
-    },
-    param: {
-      required: true,
-      type: 'string',
-      describe: 'Parameter for the job (pair, ID, etc.)'
-    },
-    interval: {
-      type: 'number',
-      default: 3000,
-      describe: 'Interval between job executions in milliseconds'
-    },
-    count: {
-      type: 'number',
-      describe: 'Number of times to run the job (omit for infinite loop)'
-    },
-    numSignatures: {
-      type: 'number',
-      default: 3,
-      describe: 'Number of oracle signatures to request for consensus'
-    },
-    gateway: {
-      type: 'string',
-      default: 'https://92.222.100.184.xip.switchboard-oracles.xyz/mainnet',
-      describe: 'Crossbar gateway URL'
-    }
-  })
-  .help()
-  .example('$0 --job binance --param BTCUSDT', 'Fetch Binance BTC/USDT via consensus')
-  .example('$0 --job binance --param BTCUSDT --numSignatures 5', 'Use 5 oracle signatures')
-  .example('$0 --job binance --param BTCUSDT --count 10', 'Run 10 times then stop')
-  .argv as any;
-
-function printJobList() {
-  console.log('\nAvailable Jobs:');
-  console.log('===============');
-  JOB_CONFIGS.forEach(config => {
-    console.log(`${config.name.padEnd(12)} - ${config.description}`);
-  });
-  console.log('');
-}
 
 
 (async function main() {
@@ -96,4 +31,5 @@ function printJobList() {
     variableOverrides: {
     },
   });
+  console.log("res", res);
 })();

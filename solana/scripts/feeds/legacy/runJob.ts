ea8453ddf6da580d9f97f54810b54471d6212f2a
Mitch Gildenberg <mitch@switchboard.xyz>
2025-09-11 20:39:38 -0400

    cp

--- solana/scripts/feeds/legacy/runJob.ts
+++ solana/scripts/feeds/legacy/runJob.ts
@@ -1,7 +1,7 @@
 import { OracleJob, CrossbarClient } from "@switchboard-xyz/common";
 import * as sb from "@switchboard-xyz/on-demand";
 
-function getJob(): OracleJob {
+function getValueJob(): OracleJob {
   const job = OracleJob.fromObject({
     tasks: [
       {
@@ -14,8 +14,29 @@ function getJob(): OracleJob {
   return job;
 }
 
+function getPolygonJob(): OracleJob {
+  const job = OracleJob.fromObject({
+    tasks: [
+      {
+        httpTask: {
+          url: "https://api.polygon.io/v2/last/trade/AAPL?apiKey=${POLYGON_API_KEY}",
+          method: "GET",
+          headers: [],
+          body: undefined,
+        }
+      },
+      {
+        jsonParseTask: {
+          path: "$.results.p",
+        }
+      }
+    ]
+  });
+  return job;
+}
+
 (async function main() {
-  const { keypair, connection, program } = await sb.AnchorUtils.loadEnv();
+  const { program } = await sb.AnchorUtils.loadEnv();
   const queue = await sb.Queue.loadDefault(program!);
   const crossbar = new CrossbarClient("http://crossbar.switchboard.xyz");
   const gateway = await queue.fetchGatewayFromCrossbar(crossbar);
@@ -23,15 +44,14 @@ function getJob(): OracleJob {
     gateway: "http://localhost:8082",
     feedConfigs: [{
       feed: {
-        jobs: [getJob()],
-        minOracleSamples: 1,
+        jobs: [getPolygonJob()],
       },
     }],
     numSignatures: 1,
     useEd25519: true,
     variableOverrides: {
-      "VALUE": "99999",
+      "POLYGON_API_KEY": process.env.POLYGON_API_KEY!,
     },
   });
-  console.log(res);
+  console.log(res.median_responses);
 })();

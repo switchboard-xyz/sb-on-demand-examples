76d5b2c9523fde5218911871866890418f0217f9
Mitch Gildenberg <mitch@switchboard.xyz>
2025-09-12 08:23:18 -0400

    cp

--- solana/programs/sb-on-demand-solana/src/lib.rs
+++ solana/programs/sb-on-demand-solana/src/lib.rs
@@ -40,6 +40,8 @@ pub mod sb_on_demand_solana {
         let cranker = state.cranker.get_or_insert(payer.key());
         let slot = get_slot(&sysvars.clock);
         // Only allow the cranker to call this function
+        //
+        // This is ONLY required if you do not verify in the same instruction
         require!(check_pubkey_eq(&cranker, payer.key), ErrorCode::ConstraintSigner);
 
         solana_program::log::sol_log_compute_units();
